7ba964bd85b98bd0f5d4d0f67e56f79a03eb54a2
Arnoldo Silva <analisi@gmail.com>
2018-03-20 17:16:58 -0300

    Implementado Classe ClashRoyale e exemplificado Atributos Estáticos

---
+++ Atributos&Metodos/ClashRoyale.cs
@@ -0,0 +1,14 @@
+namespace Atributos_Metodos
+{
+    public class ClashRoyale
+    {
+        public string nomeCarta;
+        public static double tempoDeCast;
+
+
+    public double Retornatempo(){
+        return tempoDeCast;
+    }
+
+    }
+}
--- Atributos&Metodos/Program.cs
+++ Atributos&Metodos/Program.cs
@@ -6,7 +6,39 @@ namespace Atributos_Metodos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Atributos de Classe
+
+            /* Em casos que os objetos compartilham de um mesmo atributo, chamamo-os de atributos de classe
+            um atributo de clase deve ser acessado através do nome da Classe.atributo
+
+            Ex.: No Jogo Clash Royale ao jogar uma carta existe uma tempo de mobilização, chamado de cast time, esse tempo é igual em todas as 
+            cartas de criaturas. Quando o jogo está perto de acabar o tempo de cast é reduzido (uma espécie de modo rápido de jogo) sendo assim,
+            todos os objetos do meu jogo precisarão ter o atributo tempoDeCast modificado.
+            Veja o código a seguir e compare com o codigo da classe clash royale.  
+            */
+            Console.WriteLine("Exercício Atributos de Instância");
+
+            ClashRoyale magoelEtrico = new ClashRoyale();
+            ClashRoyale principe = new ClashRoyale();
+
+            magoelEtrico.nomeCarta = "Mago Elétrico";
+            principe.nomeCarta = "Príncipe";
+
+
+            ClashRoyale.tempoDeCast = 3;
+
+            // Durante o tempo normal do jogo
+            Console.WriteLine("============ Modo Normal ======================");
+            Console.WriteLine("O " + magoelEtrico.nomeCarta + " posui " + magoelEtrico.Retornatempo() + "segundos de cast time ");
+            Console.WriteLine("O " + principe.nomeCarta + " posui " + principe.Retornatempo() + "segundos de cast time ");
+
+            //Entrando em modo jogo rápido
+
+            Console.WriteLine("============ Modo Rápido ======================");
+            ClashRoyale.tempoDeCast = 1;
+            Console.WriteLine("O " + magoelEtrico.nomeCarta + " posui " + magoelEtrico.Retornatempo() + " segundos de cast time ");
+            Console.WriteLine("O " + principe.nomeCarta + " posui " + principe.Retornatempo() + " segundos de cast time ");
+                       
         }
     }
 }

553af02164a96dbf0b94fef8475fc603b8d2ee63
Arnoldo Silva <analisi@gmail.com>
2018-03-26 20:44:39 -0300

    Comparativo entre listas e linked listas

---
+++ Generics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício Collections - Pág 144 Apostila K19");
+
+        List<int> arrayList = new List<int>();
+
+        long tempo = Program.AdicionaNoFinal(arrayList);
+        Console.WriteLine("ArrayList: " + tempo + " ms");
+
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        tempo = Program.AdicionaNoFinal(linkedList);
+        Console.WriteLine("ArrayList: " + tempo + " ms");
+           
+        }
+
+        public static long AdicionaNoFinal(ICollection<int> lista){
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            int size = 100000;
+
+            for (int i = 0; i < size; i++)
+            {
+                lista.Add(i);
+            }
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+
+
+
+
+    }
+}

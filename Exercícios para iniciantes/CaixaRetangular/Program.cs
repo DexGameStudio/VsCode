a5455da27c5e927d9109210cdcf39c50754ae3a3
Arnoldo Silva <analisi@gmail.com>
2018-03-26 22:43:15 -0300

    Exercício calcula área modificado para orientação a objetos

---
+++ Exercícios para iniciantes/CaixaRetangular/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+
+namespace CaixaRetangular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Cubo c1 = new Cubo();
+            
+            Console.WriteLine("Entre com o comprimento da caixa");
+            c1.Comprimento = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Entre com a largura da caixa ");
+            c1.Largura = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Entre com a altura da caixa");
+            c1.Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            c1.CalculaArea();
+
+            Console.WriteLine("O volume da caixa informada é "  + c1.Area + " metros cúbicos");
+
+
+            Console.WriteLine("Fim da aplicação");
+        }
+
+        public class Cubo{
+
+            public double Comprimento {get;set;}
+            public double Largura {get;set;}
+            public double Altura{get;set;}
+            public double Area{get;set;}
+        
+        
+        public void CalculaArea(){
+        
+            Area += Comprimento*Largura*Altura;
+        }
+        
+
+        }
+
+    }
+}

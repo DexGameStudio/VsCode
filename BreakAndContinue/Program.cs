34397b5cf270b40bd0cd9886eba6219d866fe8e5
Arnoldo Silva <analisi@gmail.com>
2018-04-17 21:49:13 -0300

    Break&Continue

---
+++ BreakAndContinue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BreakAndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+string[] classesJogadores = new string[]{"Humano", "Elfo", "Orc", "Morto Vivo" };
+
+    foreach (string classes in classesJogadores)
+    {
+        if(classes == "Elfo")
+        {
+            continue;
+        }
+        Console.WriteLine(classes);
+    }
+
+
+
+
+        }
+    }
+}
